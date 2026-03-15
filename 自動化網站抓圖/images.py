d96ec6dd93633b600da2efcb43b53d9efe15e7b8
oounjh <tt5303086537@gmail.com>
2026-03-16 01:52:42 +0800

    API

--- 自動化網站抓圖/images.py
+++ 自動化網站抓圖/images.py
@@ -1,11 +1,21 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = "zJWYdamQugniIebxwRyGyWUKalFNbIcZuq0Hfsg6TG4zkGAjBLxO5ZbK"
-headers = {'Authorization': API_KEY}
+API_KEY = os.environ.get('PEXELS_API_KEY')
+
+if not API_KEY:
+    raise ValueError("未偵測到 PEXELS_API_KEY 環境變數，請檢查設定！")
+
+headers = {'Authorization':API_KEY}
 
 def get_animal_photos(query='dog',per_page='5'):
     url = f'https://api.pexels.com/v1/search?query={query}&per_page={per_page}'
     response = requests.get(url,headers=headers)
+
+    if response.status_code !=200:
+        return f"Error: {response.status_code}"
+
     data = response.json()
     photos = []
     for photo in data['photos']:
